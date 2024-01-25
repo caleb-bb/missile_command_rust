43e5d8519b70af4d62969d70a365bc407dc826f8
Caleb <beerscb@gmail.com>
2024-01-25 12:45:38 -0500

    Create initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![warn(clippy::pedantic)]
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+}
+
+use prelude::*;
+
+struct State {
+    ecs: World,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut ecs = World::default();
+        Self { ecs }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
+        render_draw_buffer(ctx).expect("Render error, muthafugga")
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::new()
+        .with_title("Missile Command")
+        .with_fps_cap(30.0)
+        .with_dimensions(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .build()?;
+
+    main_loop(context, State::new())
+}
