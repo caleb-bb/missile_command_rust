f1ba4a9b77ae8bac4665f21387abfb5eb391df43
Caleb <beerscb@gmail.com>
2024-02-04 15:15:10 -0500

    Spawn a missile and make it fly downward

--- src/main.rs
+++ src/main.rs
@@ -28,21 +28,61 @@ impl Silo {
     }
 }
 
+struct Missile {
+    x: i32,
+    y: i32,
+}
+
+impl Missile {
+    fn new(x: i32, y: i32) -> Self {
+        Missile { x, y }
+    }
+
+    fn spawn_enemy_missile(rng: &mut RandomNumberGenerator, enemy_missiles: &mut Vec<Missile>) {
+        let x = rng.range(0, SCREEN_WIDTH);
+        let missile = Missile { x, y: 0 };
+        enemy_missiles.push(missile);
+    }
+
+    fn fly(&mut self) {
+        self.y += 1;
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        // ctx.set_active_console(1);
+        // ctx.cls();
+        ctx.set(self.x, self.y, RED, BLACK, to_cp437('M'));
+    }
+}
+
 struct State {
     ecs: World,
     silos: Vec<Silo>,
+    rng: RandomNumberGenerator,
+    enemy_missiles: Vec<Missile>,
 }
 
 impl State {
     fn new() -> Self {
         let mut ecs = World::default();
         let silos = vec![Silo::new(10, 49), Silo::new(40, 49), Silo::new(70, 49)];
-        Self { ecs, silos }
+        let mut enemy_missiles: Vec<Missile> = Vec::new();
+        let mut rng = RandomNumberGenerator::new();
+        Self {
+            ecs,
+            silos,
+            rng,
+            enemy_missiles,
+        }
     }
     fn play(&mut self, ctx: &mut BTerm) {
         ctx.cls_bg(BLACK);
         for silo in &mut self.silos {
             silo.render(ctx);
+        }
+        for missile in &mut self.enemy_missiles {
+            missile.render(ctx);
+            missile.fly();
         }
     }
 }
@@ -51,6 +91,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(0);
         render_draw_buffer(ctx).expect("Render error, muthafugga");
+        if self.enemy_missiles.len() < 1 {
+            Missile::spawn_enemy_missile(&mut self.rng, &mut self.enemy_missiles);
+        }
         self.play(ctx);
     }
 }
