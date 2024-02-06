6e31c3ca4586038dc16bfe83caaef716af10e26d
Caleb <beerscb@gmail.com>
2024-02-06 11:26:37 -0500

    WIP make missiles shoot at click location

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,15 @@ struct Silo {
 
 impl Silo {
     fn new(x: i32, y: i32) -> Self {
-        let missiles: Vec<Missile> = vec![Missile { x, y }; 9];
+        let missiles: Vec<Missile> = vec![
+            Missile {
+                x,
+                y,
+                destination_x: 0,
+                destination_y: 0
+            };
+            9
+        ];
         Silo { x, y, missiles }
     }
 
@@ -43,21 +51,61 @@ impl Silo {
 struct Missile {
     x: i32,
     y: i32,
+    destination_x: i32,
+    destination_y: i32,
 }
 
 impl Missile {
-    fn new(x: i32, y: i32) -> Self {
-        Missile { x, y }
+    fn new(x: i32, y: i32, destination_x: i32, destination_y: i32) -> Self {
+        Missile {
+            x,
+            y,
+            destination_x,
+            destination_y,
+        }
     }
 
     fn spawn_enemy_missile(rng: &mut RandomNumberGenerator, enemy_missiles: &mut Vec<Missile>) {
         let x = rng.range(0, SCREEN_WIDTH);
-        let missile = Missile { x, y: 0 };
+        let missile = Missile {
+            x,
+            y: 0,
+            destination_x: SCREEN_WIDTH / 2,
+            destination_y: SCREEN_HEIGHT,
+        };
         enemy_missiles.push(missile);
+    }
+
+    fn spawn_friendly_missile(
+        destination_x: i32,
+        destination_y: i32,
+        friendly_missiles: &mut Vec<Missile>,
+    ) {
+        let missile = Missile {
+            x: SCREEN_WIDTH / 2,
+            y: SCREEN_HEIGHT,
+            destination_x,
+            destination_y,
+        };
+        friendly_missiles.push(missile)
     }
 
     fn fly(&mut self) {
         self.y += 1;
+    }
+
+    fn fly_to_point(&mut self) {
+        let x_distance = self.destination_x - self.x;
+        let y_distance = self.destination_y - self.y;
+
+        if x_distance > y_distance {
+            self.y -= 1;
+            self.x -= (x_distance / y_distance)
+        }
+        if y_distance >= x_distance {
+            self.x -= 1;
+            self.y -= (y_distance / x_distance)
+        }
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
@@ -72,6 +120,7 @@ struct State {
     silos: Vec<Silo>,
     rng: RandomNumberGenerator,
     enemy_missiles: Vec<Missile>,
+    friendly_missiles: Vec<Missile>,
 }
 
 impl State {
@@ -79,12 +128,14 @@ impl State {
         let mut ecs = World::default();
         let silos = vec![Silo::new(15, 48), Silo::new(45, 48), Silo::new(75, 48)];
         let mut enemy_missiles: Vec<Missile> = Vec::new();
+        let mut friendly_missiles: Vec<Missile> = Vec::new();
         let mut rng = RandomNumberGenerator::new();
         Self {
             ecs,
             silos,
             rng,
             enemy_missiles,
+            friendly_missiles,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
@@ -96,6 +147,10 @@ impl State {
             missile.render(ctx);
             missile.fly();
         }
+        for missile in &mut (self.friendly_missiles) {
+            missile.render(ctx);
+            missile.fly_to_point();
+        }
         self.enemy_missiles
             .retain(|missile| missile.y < SCREEN_HEIGHT);
     }
@@ -106,8 +161,7 @@ impl GameState for State {
         ctx.set_active_console(0);
         let mouse = ctx.mouse_point();
         if ctx.left_click {
-            self.enemy_missiles
-                .retain(|missile| missile.x != mouse.x || missile.y != mouse.y);
+            Missile::spawn_friendly_missile(mouse.x, mouse.y, &mut self.friendly_missiles)
         }
         render_draw_buffer(ctx).expect("Render error, muthafugga");
         if self.enemy_missiles.len() < 1 {
